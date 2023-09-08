64cf7813ab2adfbbcc59ae95a7cde23d37fe4d82
Henry Lee <henryleeworld@gmail.com>
2023-09-09 06:50:06 +0800

    feat:升級至 laravel 9

--- database/migrations/2021_08_19_072133_create_sports_table.php
+++ database/migrations/2021_08_19_072133_create_sports_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSportsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -29,4 +29,4 @@ class CreateSportsTable extends Migration
     {
         Schema::dropIfExists('sports');
     }
-}
+};
--- database/migrations/2021_08_23_123135_create_country_sport_table.php
+++ database/migrations/2021_08_23_123135_create_country_sport_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCountrySportTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -29,4 +29,4 @@ class CreateCountrySportTable extends Migration
     {
         Schema::dropIfExists('country_sport');
     }
-}
+};
